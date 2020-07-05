7cb79da551e6b34cc3b1b0bcd7e23aab2bca74c0
TeAmp0is0N <rohitrathore.imh55@gmail.com>
2020-07-05 18:26:50 +0530

    Cat_vs_dogs assignment

---
+++ Tensorflow in practice/Convolutional Neural Networks in Tensorflow/Week 1/Cat_vs_dogs.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# ATTENTION: Please do not alter any of the provided code in the exercise. Only add your own code where indicated
+# ATTENTION: Please do not add or remove any cells in the exercise. The grader will check specific cells based on the cell position.
+# ATTENTION: Please use the provided epoch values when training.
+
+# In this exercise you will train a CNN on the FULL Cats-v-dogs dataset
+# This will require you doing a lot of data preprocessing because
+# the dataset isn't split into training and validation for you
+# This code block has all the required inputs
+import os
+import zipfile
+import random
+import tensorflow as tf
+import shutil
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+from os import getcwd
+
+
+# In[2]:
+
+
+path_cats_and_dogs = f"{getcwd()}/../tmp2/cats-and-dogs.zip"
+shutil.rmtree('/tmp')
+
+local_zip = path_cats_and_dogs
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+
+# In[3]:
+
+
+print(len(os.listdir('/tmp/PetImages/Cat/')))
+print(len(os.listdir('/tmp/PetImages/Dog/')))
+
+# Expected Output:
+# 1500
+# 1500
+
+
+# In[4]:
+
+
+try:
+    os.mkdir('/tmp/cats-v-dogs')
+    os.mkdir('/tmp/cats-v-dogs/training')
+    os.mkdir('/tmp/cats-v-dogs/testing')
+    os.mkdir('/tmp/cats-v-dogs/training/cats')
+    os.mkdir('/tmp/cats-v-dogs/training/dogs')
+    os.mkdir('/tmp/cats-v-dogs/testing/cats')
+    os.mkdir('/tmp/cats-v-dogs/testing/dogs')
+except OSError:
+    pass
+
+
+# In[5]:
+
+
+# Write a python function called split_data which takes
+# a SOURCE directory containing the files
+# a TRAINING directory that a portion of the files will be copied to
+# a TESTING directory that a portion of the files will be copie to
+# a SPLIT SIZE to determine the portion
+# The files should also be randomized, so that the training set is a random
+# X% of the files, and the test set is the remaining files
+# SO, for example, if SOURCE is PetImages/Cat, and SPLIT SIZE is .9
+# Then 90% of the images in PetImages/Cat will be copied to the TRAINING dir
+# and 10% of the images will be copied to the TESTING dir
+# Also -- All images should be checked, and if they have a zero file length,
+# they will not be copied over
+#
+# os.listdir(DIRECTORY) gives you a listing of the contents of that directory
+# os.path.getsize(PATH) gives you the size of the file
+# copyfile(source, destination) copies a file from source to destination
+# random.sample(list, len(list)) shuffles a list
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+    files = []
+    for filename in os.listdir(SOURCE):
+        file = SOURCE + filename
+        if os.path.getsize(file) > 0:
+            files.append(filename)
+        else:
+            print(filename + " is zero length, so ignoring.")
+
+    training_length = int(len(files) * SPLIT_SIZE)
+    testing_length = int(len(files) - training_length)
+    shuffled_set = random.sample(files, len(files))
+    training_set = shuffled_set[0:training_length]
+    testing_set = shuffled_set[-testing_length:]
+
+    for filename in training_set:
+        this_file = SOURCE + filename
+        destination = TRAINING + filename
+        copyfile(this_file, destination)
+
+    for filename in testing_set:
+        this_file = SOURCE + filename
+        destination = TESTING + filename
+        copyfile(this_file, destination)
+
+
+CAT_SOURCE_DIR = "/tmp/PetImages/Cat/"
+TRAINING_CATS_DIR = "/tmp/cats-v-dogs/training/cats/"
+TESTING_CATS_DIR = "/tmp/cats-v-dogs/testing/cats/"
+DOG_SOURCE_DIR = "/tmp/PetImages/Dog/"
+TRAINING_DOGS_DIR = "/tmp/cats-v-dogs/training/dogs/"
+TESTING_DOGS_DIR = "/tmp/cats-v-dogs/testing/dogs/"
+
+split_size = .9
+split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+# Expected output
+# 666.jpg is zero length, so ignoring
+# 11702.jpg is zero length, so ignoring
+
+
+# In[6]:
+
+
+print(len(os.listdir('/tmp/cats-v-dogs/training/cats/')))
+print(len(os.listdir('/tmp/cats-v-dogs/training/dogs/')))
+print(len(os.listdir('/tmp/cats-v-dogs/testing/cats/')))
+print(len(os.listdir('/tmp/cats-v-dogs/testing/dogs/')))
+
+# Expected output:
+# 1350
+# 1350
+# 150
+# 150
+
+
+# In[7]:
+
+
+# DEFINE A KERAS MODEL TO CLASSIFY CATS V DOGS
+# USE AT LEAST 3 CONVOLUTION LAYERS
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
+
+
+# # NOTE:
+# 
+# In the cell below you **MUST** use a batch size of 10 (`batch_size=10`) for the `train_generator` and the `validation_generator`. Using a batch size greater than 10 will exceed memory limits on the Coursera platform.
+
+# In[8]:
+
+
+TRAINING_DIR = "/tmp/cats-v-dogs/training/"
+train_datagen = ImageDataGenerator(rescale=1.0/255.)
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                    batch_size=100,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+VALIDATION_DIR = "/tmp/cats-v-dogs/testing/"
+validation_datagen = ImageDataGenerator(rescale=1.0/255.)
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                              batch_size=100,
+                                                              class_mode='binary',
+                                                              target_size=(150, 150))
+
+# Expected Output:
+# Found 22498 images belonging to 2 classes.
+# Found 2500 images belonging to 2 classes.
+
+
+# In[9]:
+
+
+history = model.fit_generator(train_generator,
+                              epochs=2,
+                              verbose=1,
+                              validation_data=validation_generator)
+
+
+# In[10]:
+
+
+# PLOT LOSS AND ACCURACY
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+
+
+plt.title('Training and validation loss')
+
+# Desired output. Charts with training and validation metrics. No crash :)
+
+
+# # Submission Instructions
+
+# In[11]:
+
+
+# Now click the 'Submit Assignment' button above.
+
+
+# # When you're done or would like to take a break, please run the two cells below to save your work and close the Notebook. This will free up resources for your fellow learners. 
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('javascript', '', '<!-- Save the notebook -->\nIPython.notebook.save_checkpoint();')
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('javascript', '', 'IPython.notebook.session.delete();\nwindow.onbeforeunload = null\nsetTimeout(function() { window.close(); }, 1000);')
+
